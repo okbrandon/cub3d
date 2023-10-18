6fbb5ea3628546e66a6a697b3a1b1ad6f3d4b512
okbrandon <okbrandonlooksdumb@gmail.com>
2023-10-18 11:17:41 +0200

    :recycle: Moved floor/ceiling draw functions

--- srcs/ft_drawer.c
+++ srcs/ft_drawer.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_drawer.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: evmorvan <evmorvan@student.42nice.fr>      +#+  +:+       +#+        */
+/*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 19:37:43 by bsoubaig          #+#    #+#             */
-/*   Updated: 2023/10/17 14:30:30 by evmorvan         ###   ########.fr       */
+/*   Updated: 2023/10/18 11:16:42 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,22 @@ static void	ft_draw_vertical_line(t_cub *cub, int x, int from, int to, int color
 			ft_mlx_pixel_put(cub, x, y, color);
 		else
 			ft_mlx_pixel_put(cub, x, y, ft_to_trgb(0, 0, 0, 0));
+	}
+}
+
+static void	ft_draw_floor_and_ceiling(t_cub *cub, int x)
+{
+	float	ds;
+	int		y;
+
+	ds = ((float)WIN_HEIGHT / 2) - ((float)cub->raycast.line_height / 2);
+	y = -1;
+	while (++y < WIN_HEIGHT)
+	{
+		if (y < ds)
+			ft_mlx_pixel_put(cub, x, y, cub->textures.ceiling);
+		else if (y >= ds + cub->raycast.line_height)
+			ft_mlx_pixel_put(cub, x, y, cub->textures.floor);
 	}
 }
 
@@ -48,4 +64,5 @@ void	ft_draw_textures(t_cub *cub, int x)
 			ft_draw_vertical_line(cub, x, cub->raycast.draw_from, \
 				cub->raycast.draw_to, cub->textures.north);
 	}
+	ft_draw_floor_and_ceiling(cub, x);
 }
--- srcs/ft_raycaster.c
+++ srcs/ft_raycaster.c
@@ -6,7 +6,7 @@
 /*   By: bsoubaig <bsoubaig@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 12:22:53 by bsoubaig          #+#    #+#             */
-/*   Updated: 2023/10/18 10:53:28 by bsoubaig         ###   ########.fr       */
+/*   Updated: 2023/10/18 11:16:47 by bsoubaig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,22 +105,6 @@ static void	ft_calc_line_height(t_cub *cub)
 		ray->draw_to = WIN_HEIGHT - 1;
 }
 
-static void	ft_floorcast(t_cub *cub, int x)
-{
-	float	ds;
-	int		y;
-
-	ds = ((float)WIN_HEIGHT / 2) - ((float)cub->raycast.line_height / 2);
-	y = -1;
-	while (++y < WIN_HEIGHT)
-	{
-		if (y < ds)
-			ft_mlx_pixel_put(cub, x, y, cub->textures.ceiling);
-		else if (y >= ds + cub->raycast.line_height)
-			ft_mlx_pixel_put(cub, x, y, cub->textures.floor);
-	}
-}
-
 void	ft_raycast(t_cub *cub)
 {
 	int	x;
@@ -133,6 +117,5 @@ void	ft_raycast(t_cub *cub)
 		ft_apply_dda_algorithm(cub);
 		ft_calc_line_height(cub);
 		ft_draw_textures(cub, x);
-		ft_floorcast(cub, x);
 	}
 }
